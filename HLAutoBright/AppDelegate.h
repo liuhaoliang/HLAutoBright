02fa7e0cb7a96adbdd612ed9424567a905b16445
刘豪亮 <271072547@qq.com>
2017-07-12 18:21:20 +0800

    initial

---
+++ HLAutoBright/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLAutoBright
+//
+//  Created by 海军 on 2017/7/5.
+//  Copyright © 2017年 LHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HLAutoBright/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLAutoBright
+//
+//  Created by 海军 on 2017/7/5.
+//  Copyright © 2017年 LHL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
